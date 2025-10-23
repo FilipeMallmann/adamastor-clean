0c39bea48ddcfd14131bdb0e07aa8cdcb98f0f50
FilipeMallmann <filipe.pm@gmail.com>
2025-10-23 23:59:44 +0100

    assets e outras coisas

--- scripts/managers/morale_deck_manager.gd
+++ scripts/managers/morale_deck_manager.gd
@@ -20,8 +20,26 @@ func put_at_botton(card: PlayerCard) -> void:
 func put_at_top(card :PlayerCard) -> void:
 	deck.push_front(card)
 
-func load_cards():
-	pass
+func _create_card(card_info : CardData) -> PlayerCard:
+	var card = PlayerCard.new()
+	card.card_data = card_info
+	return card
+
+func load_cards(path) -> void:
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+				deck.append(_create_card(file_name as CardData))
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	shufle()
 
 func get_total_cards() -> int:
 	return deck.size()
--- scripts/managers/navigation_manager.gd
+++ scripts/managers/navigation_manager.gd
@@ -1,11 +1,11 @@
 extends Node
+class_name NavigationManager
 
+signal reach_destination
+enum NavigationHexagonType { Clear, Fog, Island, AgainstTide, OnTide, FinalDestination}
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func navigate() -> void:
 	pass
+
+func calculate_navigation_cost(terrain: NavigationHexagonType) -> int:
+	return 0
